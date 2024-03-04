4501c575cc1c6e57123eb39b5328d22fc0abc0cb
moavalos <mora_m_avalos@hotmail.com>
2024-03-03 23:48:54 -0300

    first commit

---
+++ music.app/src/main/java/com/melodycrazy/music/app/MelodyCrazeApplication.java
@@ -0,0 +1,13 @@
+package com.melodycrazy.music.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyCrazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyCrazeApplication.class, args);
+	}
+
+}
---
+++ music.app/src/test/java/com/melodycrazy/music/app/MelodyCrazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melodycrazy.music.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodyCrazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
